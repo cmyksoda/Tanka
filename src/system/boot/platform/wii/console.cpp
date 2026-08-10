be1a0e09be23ee0a366b5e558ca7d0f80c8dd03f
cmyksoda <jaxi@cmyksoda.cc>
2026-08-10 18:20:58 -0400

    wii: Fix console compilation by adding missing methods and removing extern C

--- src/system/boot/platform/wii/console.cpp
+++ src/system/boot/platform/wii/console.cpp
@@ -14,11 +14,17 @@ class Console : public ConsoleNode {
 			size_t bufferSize) { return B_OK; }
 		virtual ssize_t WriteAt(void *cookie, off_t pos, const void *buffer,
 			size_t bufferSize) { return bufferSize; }
+		virtual void ClearScreen() {}
+		virtual int32 Width() { return 80; }
+		virtual int32 Height() { return 25; }
+		virtual void SetCursor(int32 x, int32 y) {}
+		virtual void SetCursorVisible(bool visible) {}
+		virtual void SetColors(int32 foreground, int32 background) {}
 };
 
 static Console sConsole;
 
-extern "C" status_t
+status_t
 platform_init_video(void)
 {
 	stdout = stderr = (FILE *)&sConsole;
@@ -26,57 +32,58 @@ platform_init_video(void)
 }
 
 
-extern "C" void
+void
 platform_switch_to_logo(void)
 {
 }
 
 
-extern "C" void
+void
 platform_switch_to_text_mode(void)
 {
 }
 
 
-extern "C" void
+void
 platform_cleanup_devices()
 {
 }
 
-extern "C" void
+void
 platform_run_menu(Menu *menu)
 {
 }
 
-extern "C" void
+void
 platform_update_menu_item(Menu *menu, MenuItem *item)
 {
 }
 
-extern "C" size_t
+size_t
 platform_get_user_input_text(Menu *menu, MenuItem *item, char *buffer,
 	size_t bufferSize)
 {
 	return 0;
 }
 
-extern "C" void
+void
 platform_add_menus(Menu *menu)
 {
 }
 
-extern "C" char *
+char *
 platform_debug_get_log_buffer(size_t *_size)
 {
 	return NULL;
 }
 
-extern "C" void
+void
 platform_debug_syslog_early(const char *buffer, size_t length)
 {
 }
 
-extern "C" void
+void
 platform_load_ucode(BootVolume &volume)
 {
 }
+
