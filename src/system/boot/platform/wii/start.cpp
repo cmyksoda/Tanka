139b4d0863bbecb67af18b882c171e16a889c3ed
cmyksoda <jaxi@cmyksoda.cc>
2026-08-10 01:01:00 -0400

    wii: Implement platform_start_kernel assembly handoff

--- src/system/boot/platform/wii/start.cpp
+++ src/system/boot/platform/wii/start.cpp
@@ -24,10 +24,39 @@ extern "C" int main(stage2_args *args);
 static void *xfb = NULL;
 static GXRModeObj *rmode = NULL;
 
+static addr_t
+get_kernel_entry(void)
+{
+	if (gKernelArgs.kernel_image->elf_class == ELFCLASS64) {
+		preloaded_elf64_image *image = static_cast<preloaded_elf64_image *>(
+			gKernelArgs.kernel_image.Pointer());
+		return image->elf_header.e_entry;
+	} else if (gKernelArgs.kernel_image->elf_class == ELFCLASS32) {
+		preloaded_elf32_image *image = static_cast<preloaded_elf32_image *>(
+			gKernelArgs.kernel_image.Pointer());
+		return image->elf_header.e_entry;
+	}
+	panic("Unknown kernel format! Not 32-bit or 64-bit!");
+	return 0;
+}
+
 extern "C" void
 platform_start_kernel(void)
 {
-	panic("platform_start_kernel not implemented on Wii yet!");
+	addr_t kernelEntry = get_kernel_entry();
+	addr_t stackTop = gKernelArgs.cpu_kstack[0].start
+		+ gKernelArgs.cpu_kstack[0].size;
+
+	printf("kernel entry at %p\n", (void*)kernelEntry);
+	printf("kernel stack top: %p\n", (void*)stackTop);
+
+	// On the Wii, libogc has interrupts enabled. 
+	// The kernel expects them to be disabled before handoff.
+	_CPU_ISR_Disable();
+
+	status_t error = arch_start_kernel(&gKernelArgs, kernelEntry, stackTop);
+
+	panic("Kernel returned! Return value: %" B_PRId32 "\n", error);
 }
 
 extern "C" void
