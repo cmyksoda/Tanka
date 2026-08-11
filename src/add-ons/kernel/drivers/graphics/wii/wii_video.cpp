0eeba672a20bc6bcd8925b00a36b75a07f96ffc5
cmyksoda <jaxi@cmyksoda.cc>
2026-08-11 00:40:04 -0400

    wii: Implement framebuffer video driver with accelerant support
    Map the boot framebuffer and create vesa_shared_info for the
    framebuffer.accelerant. Handle B_GET_ACCELERANT_SIGNATURE,
    VESA_GET_PRIVATE_DATA, VESA_CLONE_FRAME_BUFFER, and
    VESA_GET_DEVICE_NAME ioctls. Use write-combining memory for
    the framebuffer mapping on Broadway.

--- src/add-ons/kernel/drivers/graphics/wii/wii_video.cpp
+++ src/add-ons/kernel/drivers/graphics/wii/wii_video.cpp
@@ -11,15 +11,125 @@
 #include <Errors.h>
 #include <string.h>
 
+#include <boot_item.h>
+#include <frame_buffer_console.h>
+#include <vm/vm.h>
+#include <graphic_driver.h>
+
+#include <vesa_info.h>
+
 #define WII_VIDEO_DEVICE_NAME "graphics/wii_video"
+#define WII_ACCELERANT_NAME "framebuffer.accelerant"
 
 int32 api_version = B_OS_API_VERSION;
+
+struct wii_video_info {
+	vesa_shared_info*	shared_info;
+	area_id				shared_area;
+	area_id				frame_buffer_area;
+	addr_t				frame_buffer;
+	int32				id;
+	int32				open_count;
+};
+
+static wii_video_info sInfo;
+
+static uint32
+get_color_space_for_depth(uint32 depth)
+{
+	switch (depth) {
+		case 8: return B_CMAP8;
+		case 15: return B_RGB15;
+		case 16: return B_RGB16;
+		case 24: return B_RGB24;
+		case 32: return B_RGB32;
+	}
+	return 0;
+}
+
+
+static status_t
+wii_video_init_info()
+{
+	frame_buffer_boot_info* bufferInfo
+		= (frame_buffer_boot_info*)get_boot_item(FRAME_BUFFER_BOOT_INFO, NULL);
+	if (bufferInfo == NULL)
+		return B_ERROR;
+
+	size_t sharedSize = (sizeof(vesa_shared_info) + 7) & ~7;
+
+	sInfo.shared_area = create_area("wii video shared info",
+		(void**)&sInfo.shared_info, B_ANY_KERNEL_ADDRESS,
+		ROUND_TO_PAGE_SIZE(sharedSize), B_FULL_LOCK,
+		B_KERNEL_READ_AREA | B_KERNEL_WRITE_AREA | B_CLONEABLE_AREA);
+	if (sInfo.shared_area < 0)
+		return sInfo.shared_area;
+
+	memset(sInfo.shared_info, 0, sizeof(vesa_shared_info));
+
+	// Map the framebuffer with write-combining for better performance
+	// on the Broadway CPU (gathered stores vs. strictly ordered bus writes)
+	addr_t frameBuffer;
+	size_t fbSize = bufferInfo->bytes_per_row * bufferInfo->height;
+
+	sInfo.frame_buffer_area = map_physical_memory("wii video framebuffer",
+		bufferInfo->physical_frame_buffer, fbSize,
+		B_ANY_KERNEL_ADDRESS | B_WRITE_COMBINING_MEMORY,
+		B_READ_AREA | B_WRITE_AREA, (void**)&frameBuffer);
+	if (sInfo.frame_buffer_area < 0) {
+		delete_area(sInfo.shared_area);
+		return sInfo.frame_buffer_area;
+	}
+
+	sInfo.frame_buffer = frameBuffer;
+	vm_set_area_memory_type(sInfo.frame_buffer_area,
+		bufferInfo->physical_frame_buffer, B_WRITE_COMBINING_MEMORY);
+
+	vesa_shared_info& sharedInfo = *sInfo.shared_info;
+	sharedInfo.current_mode.virtual_width = bufferInfo->width;
+	sharedInfo.current_mode.virtual_height = bufferInfo->height;
+	sharedInfo.current_mode.space = get_color_space_for_depth(bufferInfo->depth);
+	sharedInfo.bytes_per_row = bufferInfo->bytes_per_row;
+	strlcpy(sharedInfo.name, "Nintendo Wii", sizeof(sharedInfo.name));
+
+	// EDID is not available on Wii
+	sharedInfo.has_edid = false;
+
+	frame_buffer_update(frameBuffer, bufferInfo->width, bufferInfo->height,
+		bufferInfo->depth, bufferInfo->bytes_per_row);
+
+	dprintf("wii_video: initialized %ux%ux%u framebuffer\n",
+		bufferInfo->width, bufferInfo->height, bufferInfo->depth);
+
+	return B_OK;
+}
+
+
+static void
+wii_video_uninit_info()
+{
+	if (sInfo.frame_buffer_area >= 0) {
+		vm_change_clones_to_null_areas(sInfo.frame_buffer_area);
+		delete_area(sInfo.frame_buffer_area);
+	}
+	if (sInfo.shared_area >= 0)
+		delete_area(sInfo.shared_area);
+}
 
 
 static status_t
 wii_video_open(const char* name, uint32 flags, void** cookie)
 {
-	*cookie = NULL;
+	status_t status = B_OK;
+
+	if (sInfo.open_count == 0) {
+		status = wii_video_init_info();
+		if (status != B_OK)
+			return status;
+	}
+
+	sInfo.open_count++;
+	*cookie = &sInfo;
 	return B_OK;
 }
 
@@ -34,6 +144,8 @@ wii_video_close(void* cookie)
 static status_t
 wii_video_free(void* cookie)
 {
+	if (--sInfo.open_count == 0)
+		wii_video_uninit_info();
 	return B_OK;
 }
 
@@ -55,11 +167,43 @@ wii_video_write(void* cookie, off_t position, const void* buf, size_t* num_bytes
 
 
 static status_t
-wii_video_control(void* cookie, uint32 op, void* arg, size_t length)
+wii_video_control(void* cookie, uint32 op, void* buffer, size_t length)
 {
-	// TODO: Handle accelerant ioctls (B_GET_ACCELERANT_SIGNATURE, etc.)
-	// TODO: Handle custom ioctl for GX hardware conversion of RGB to YCbCr
-	return B_BAD_VALUE;
+	wii_video_info* info = (wii_video_info*)cookie;
+
+	switch (op) {
+		case B_GET_ACCELERANT_SIGNATURE:
+			dprintf("wii_video: accelerant signature: %s\n", WII_ACCELERANT_NAME);
+			if (user_strlcpy((char*)buffer, WII_ACCELERANT_NAME,
+					B_FILE_NAME_LENGTH) < B_OK)
+				return B_BAD_ADDRESS;
+			return B_OK;
+
+		case VESA_GET_PRIVATE_DATA:
+			return user_memcpy(buffer, &info->shared_area, sizeof(area_id));
+
+		case VESA_CLONE_FRAME_BUFFER:
+		{
+			void* dummy;
+			area_id area = vm_clone_area(B_CURRENT_TEAM, "wii cloned framebuffer",
+				&dummy, B_ANY_ADDRESS, B_READ_AREA | B_WRITE_AREA, 0,
+				info->frame_buffer_area, true);
+			if (area < 0)
+				return area;
+			return _user_get_area_info(area, (area_info*)buffer);
+		}
+
+		case VESA_GET_DEVICE_NAME:
+			if (user_strlcpy((char*)buffer, WII_VIDEO_DEVICE_NAME,
+					B_PATH_NAME_LENGTH) < B_OK)
+				return B_BAD_ADDRESS;
+			return B_OK;
+
+		default:
+			break;
+	}
+
+	return B_DEV_INVALID_IOCTL;
 }
 
 
@@ -72,7 +216,10 @@ static const char* sDeviceNames[] = {
 extern "C" status_t
 init_hardware(void)
 {
-	// TODO: Check if we are running on a Wii
+	frame_buffer_boot_info* bufferInfo
+		= (frame_buffer_boot_info*)get_boot_item(FRAME_BUFFER_BOOT_INFO, NULL);
+	if (bufferInfo == NULL)
+		return B_ERROR;
 	return B_OK;
 }
 
@@ -80,6 +227,9 @@ init_hardware(void)
 extern "C" status_t
 init_driver(void)
 {
+	memset(&sInfo, 0, sizeof(sInfo));
+	sInfo.shared_area = -1;
+	sInfo.frame_buffer_area = -1;
 	return B_OK;
 }
 
